95ce967a40b89aa61e46d82f17aae921fe34d03c
Shahmir Khan <kshahmir822@gmail.com>
2025-01-17 09:52:30 +0500

    leetcode problems

---
+++ 1_two_sum.py
@@ -0,0 +1,21 @@
+class solution:
+
+   def two_sum(self,nums,targt):
+     
+      found = False
+     
+      for i in range(len(nums)):
+         
+         for j in range(len(nums)):
+            if(nums[i]+nums[j]==target):
+               found = True
+               break
+       
+         if(found):
+            return i,j
+
+nums = [1,2,3,4] # list
+target = 7  # element to found after adding two list elements
+s1 = solution()
+print(s1.two_sum(nums,target))      
+            
