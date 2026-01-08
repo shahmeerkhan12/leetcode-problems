77c4c73bb4f1254af6a04f23b1d6e2d12e41d843
Shahmir Khan <100615405+shahmeerkhan12@users.noreply.github.com>
2026-01-08 22:57:52 +0500

    Optimize removeDuplicates function implementation
    Optimize the removeDuplicates function for better performance.

--- 80_remove_duplicates_ii.py
+++ 80_remove_duplicates_ii.py
@@ -1,7 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        // optimized solution
+        n = len(nums)
+        // basic/initial check for immediate exit
+        if n <= 2:
+            return n
         valid_elements = 2
-        for cur in range(2,len(nums):
+        for cur in range(2,n):
             if nums[valid_elements-2] != nums[cur]:
                 nums[valid_elements] = nums[cur]
                 valid_elements += 1
