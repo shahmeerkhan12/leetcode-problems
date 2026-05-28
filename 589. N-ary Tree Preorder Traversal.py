0f9440cf8228976f46cf0d5e30d687d0dae839e7
Shahmir Khan <100615405+shahmeerkhan12@users.noreply.github.com>
2026-05-29 00:00:17 +0500

    Implement N-ary Tree Preorder Traversal

---
+++ 589. N-ary Tree Preorder Traversal.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+    
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        res = []
+
+        def dfs(node):
+            if not node:
+                return
+            
+            res.append(node.val)
+            for child in node.children:
+                
+                dfs(child)
+            
+        dfs(root)
+        return res
